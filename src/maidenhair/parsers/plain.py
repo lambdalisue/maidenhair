0ed03f239ae786e7228dcbb5da47d54e47a5555d
Λlisue <lambdalisue@hashnote.net>
2014-05-31 14:42:15 +0900

    Add CSVParser

--- src/maidenhair/parsers/plain.py
+++ src/maidenhair/parsers/plain.py
@@ -9,6 +9,7 @@ __author__  = 'Alisue (lambdalisue@hashnote.net)'
 import numpy as np
 from maidenhair.parsers.base import BaseParser
 
+
 class PlainParser(BaseParser):
     """
     A plain text parser class based on `numpy.loadtxt` method
@@ -30,3 +31,26 @@ class PlainParser(BaseParser):
 
         """
         return np.loadtxt(iterable, **kwargs)
+
+
+class CSVParser(BaseParser):
+    """
+    A CSV text parser class based on `numpy.loadtxt` method
+    """
+    def parse(self, iterable, **kwargs):
+        """
+        Parse whitespace separated iterable to a numpy array.
+        It is based on `numpy.loadtxt` method.
+
+        Parameters
+        ----------
+            iterable : iterable
+                An iterable instance to parse
+
+        Returns
+        -------
+        ndarray
+            An instance of numpy array
+
+        """
+        return np.loadtxt(iterable, delimiter=',', **kwargs)
